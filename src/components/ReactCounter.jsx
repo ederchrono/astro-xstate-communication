66a4d5c6b8a7563a050296fdd9cc586b78baaebb
Eder Díaz <ederchrono@gmail.com>
2022-06-30 22:07:07 -0500

    astro project with vue and react component

---
+++ src/components/ReactCounter.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+
+function ReactCounter() {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <p>You clicked {count} times</p>
+      <button onClick={() => setCount(count + 1)}>
+        Click me
+      </button>
+    </div>
+  );
+}
+
+export default ReactCounter;
---
+++ src/components/VueCounter.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  }
+}
+</script>
+
+<template>
+  <div>
+    <p>You clicked {{count}} times</p>
+    <button @click="count++">Click me</button>
+  </div>
+</template>
